a6a782651db9eb47df022134c8658220cbd915dd
usuario <usuario@10.22.147.138>
2014-04-23 10:08:44 +0100

    Test anotherClassDependencies passed.

--- test/classDependencies/ClassDependencyEvaluator.java
+++ test/classDependencies/ClassDependencyEvaluator.java
@@ -1,15 +1,21 @@
 package classDependencies;
 
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ClassDependencyEvaluator {
 
     public ArrayList<String> getDependencies(String file) {
-        return new ArrayList<String>() {
-            {
-                add("evaluator.Expression");
-            }
-        };
+        ArrayList<String> dependencies = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            if(line.contains("import"))
+                dependencies.add(line.split(" ")[1].split(";")[0]);
+        }
+        return dependencies;
     }
     
 }
