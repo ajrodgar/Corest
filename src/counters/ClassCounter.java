c2e56c0de5ffe5b27ef6487bc2b84a8d8ba1be82
ramclen <ramclenlp@gmail.com>
2014-04-25 11:23:05 +0100

    filesAndClassesInADirectoryWithMoreDirectories Test Completed

--- src/counters/ClassCounter.java
+++ src/counters/ClassCounter.java
@@ -1,6 +1,7 @@
 package counters;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,8 +9,27 @@ public class ClassCounter implements Counter{
 
     @Override
     public int count(File file) {
-        return file.listFiles().length;
+        int classNumber = countClassInDirectory(file);
+        for (File directory : getDirectoriesList(file))
+            classNumber+= count(directory);
+        return classNumber;
     }
 
+    private List<File> getDirectoriesList(File externalDirectory) {
+        List<File> list = new LinkedList<File>();
+        for (File file: externalDirectory.listFiles())
+            if (file.isDirectory())
+                list.add(file);
+        return list;
+    }
+
+    private int countClassInDirectory(File directory){
+        return directory.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File directory, String nameFile) {
+                return nameFile.contains(".java");
+            }
+        }).length;
+    }
 
 }
--- test/counters/ClassCounterTest.java
+++ test/counters/ClassCounterTest.java
@@ -13,4 +13,16 @@ public class ClassCounterTest {
         Assert.assertEquals(3, counter.count(new File("resource/only.classes.in.only.one.directory")));
     }
 
+    @Test
+    public void filesAndClassesInOnlyOneDirectoryTest() {
+        Counter counter = new ClassCounter();
+        Assert.assertEquals(3, counter.count(new File("resource/files.and.classes.in.only.one.directory")));
+    }
+
+    @Test
+    public void filesAndClassesInADirectoryWithMoreDirectoriesTest() {
+        Counter counter = new ClassCounter();
+        Assert.assertEquals(3, counter.count(new File("resource/files.and.classes.in.directory.with.more.directories")));
+    }
+
 }
