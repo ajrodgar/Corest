522ff0c874554bbca7a10413aef6b51b28bcc282
usuario <usuario@10.22.147.153>
2014-04-28 14:32:02 +0100

    Working example - AnalyzerTest (Analyzer interface).

---
+++ src/analyzer/AnalyzerTest.java
@@ -0,0 +1,27 @@
+package analyzer;
+
+import main.MyResult;
+
+
+public class AnalyzerTest implements Analyzer{
+    String source;
+    String resultTXT;
+    
+    public AnalyzerTest(String src) {
+        
+        this.source = src;
+        calculateResultTXT();
+    }
+    
+    private void calculateResultTXT(){
+        
+        resultTXT=source;
+    }
+    
+    @Override
+    public MyResult getResult() {
+        
+        return new MyResult("\nAnalyzerTest src a analizar: "+resultTXT);
+    }
+    
+}
