f3ed4701d7decb104b3bb8836aaa9113d4fd1eea
Óscar Antonio González de Chaves Pérez <oscar.agcp@gmail.com>
2014-04-30 10:27:28 +0100

    trends package added

---
+++ src/trends/ApiGoogleTrends.java
@@ -0,0 +1,46 @@
+
+package trends;
+
+import javax.naming.ConfigurationException;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.freaknet.gtrends.api.GoogleAuthenticator;
+import org.freaknet.gtrends.api.GoogleTrendsClient;
+import org.freaknet.gtrends.api.GoogleTrendsCsvParser;
+import org.freaknet.gtrends.api.GoogleTrendsRequest;
+import org.freaknet.gtrends.api.exceptions.GoogleTrendsClientException;
+import org.freaknet.gtrends.api.exceptions.GoogleTrendsRequestException;
+
+public class ApiGoogleTrends {
+    public void getTopCities(String query) throws GoogleTrendsClientException, GoogleTrendsRequestException, ConfigurationException, org.apache.commons.configuration.ConfigurationException {
+        String u = "codestproject@gmail.com";
+        String p = "proyectogs2";
+
+        /* OPTIONAL: setup a proxy with NTLM authentication */
+        HttpHost proxy = new HttpHost("222.124.198.136", 3129, "http");
+        Credentials credentials = new NTCredentials("", "", "", "DOMAIN");
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        httpClient.getCredentialsProvider().setCredentials(new AuthScope(proxy.getHostName(), proxy.getPort()), credentials);
+        httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+
+        /* Creates a new authenticator */
+        GoogleAuthenticator authenticator = new GoogleAuthenticator(u, p, httpClient);
+
+        /* Creates a new Google Trends Client */
+        GoogleTrendsClient client = new GoogleTrendsClient(authenticator, httpClient);
+        GoogleTrendsRequest request = new GoogleTrendsRequest(query);
+
+        /* Here the default request params can be modified with getter/setter methods */
+        String content = client.execute(request);
+
+        /* The default request downloads a CSV available in content */
+        GoogleTrendsCsvParser csvParser = new GoogleTrendsCsvParser(content);
+        /* Get a specific section of the CSV */
+        String section = csvParser.getSectionAsString("Top cities for", true);
+        System.out.println(section);
+    }
+}
---
+++ src/trends/ApiTwitter.java
@@ -0,0 +1,32 @@
+package trends;
+
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class ApiTwitter {
+
+    private static final int countMax = 100;
+
+    public int numTweets(String q, String since, String until, GeoLocation geoLocation, int radio) throws TwitterException {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("R6pHmw0qdDWgXH7MEu40fskl4")
+                .setOAuthConsumerSecret("gas9ZH9jzrVB2yM3Xhn6jtt1sdszIWhlc2MyZaQ96E9voCwdJ0")
+                .setOAuthAccessToken("14117909-GE2XmU3Qi5fdip46OQQahuZGR53EZ3f11Q8m5Y0vQ")
+                .setOAuthAccessTokenSecret("Gpmid7SrxxKTCZDHSkHiiGVi6S5ghdnvaoedGtij2Utcm");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
+        // The factory instance is re-useable and thread safe.
+        Query query = new Query(q);
+
+        query.setCount(countMax);
+        query.setSince(since);
+        query.setUntil(until);
+        query.geoCode(new GeoLocation(28.113155, -15.440883), radio, "km");
+
+        QueryResult result = twitter.search(query);
+
+        return result.getTweets().size();
+    }
+}
