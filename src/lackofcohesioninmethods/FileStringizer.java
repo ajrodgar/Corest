9fb297372f9626497bc391e3999cb52f37adb3ba
usuario <usuario@10.22.147.248>
2014-05-02 10:58:12 +0100

    Comments deleted

--- src/lackofcohesioninmethods/FileStringizer.java
+++ src/lackofcohesioninmethods/FileStringizer.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 
 public class FileStringizer {
 
-    private static String fileToString(File file) {
+    public static String fileToString(File file) {
         FileReader fr = null;
         BufferedReader br = null;
         String fileString = "";
@@ -16,16 +16,17 @@ public class FileStringizer {
             br = new BufferedReader(fr);
             String linea;
             while ((linea = br.readLine()) != null) {
+                linea = deleteComments(linea);
                 fileString += linea;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return fileString;
+        return deleteMultiLineComments(fileString);
     }
 
-    private static String format(String file) {
-        String formatedFile = file.toString();
+    private static String format(String source) {
+        String formatedFile = source.toString();
         formatedFile = formatedFile.replace("{", "{\n");
         formatedFile = formatedFile.replace(";", ";\n");
         formatedFile = formatedFile.replace("}", "\n}\n");
@@ -35,5 +36,19 @@ public class FileStringizer {
     public static String[] prepareFile(File file) {
         String code = format(FileStringizer.fileToString(file));
         return code.split("\n");
+    }
+    
+    private static String deleteComments(String line){
+        if(line.contains("//")) return line.substring(0, line.indexOf("//"));
+        return line;
+    }
+    
+    private static String deleteMultiLineComments(String source){
+        while(source.contains("/*")){
+            int start = source.indexOf("/*");
+            int end = source.indexOf("*/") + 2;
+            source = source.substring(0, start) + source.substring(end, source.length());
+        }
+        return source;
     }
 }
--- src/lackofcohesioninmethods/Main.java
+++ src/lackofcohesioninmethods/Main.java
@@ -1,8 +1,13 @@
 package lackofcohesioninmethods;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        String[] code = FileStringizer.prepareFile(new File("simpleClass.txt"));
+        for (String string : code) {
+            System.out.println(string);
+        }
     }
 }
