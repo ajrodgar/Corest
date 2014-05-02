946e96cd4fc26cfe16a5ceb06e0d8a3bfcf29511
rubendm92 <rubendm92@gmail.com>
2014-05-02 13:33:24 +0100

    Delete DependencyDictionary interface

--- src/corest/rank/DependencyDictionary.java
+++
@@ -1,8 +0,0 @@
-package corest.rank;
-
-import java.util.ArrayList;
-
-public interface DependencyDictionary {
-    
-    public ArrayList<String> getDependentClasses(String file, String className);
-}
