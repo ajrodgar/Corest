7731c5ea4bb476dd59307237672284ec585125a4
Oscar <oscarverona@gmail.com>
2014-05-08 00:22:15 +0100

    Private and branch options

--- webContent/static/js/main.js
+++ webContent/static/js/main.js
@@ -1,11 +1,13 @@
 jQuery(function($) {
-
 	//#main-slider
 	$(function(){
 		$('#main-slider.carousel').carousel({
 			interval: 8000
 		});
 	});
+
+	//#private fields
+	$("#private-fields, #advanced-fields").hide().removeClass('hide');
 
 	$( '.centered' ).each(function( e ) {
 		$(this).css('margin-top',  ($('#main-slider').height() - $(this).height())/2);
@@ -27,25 +29,50 @@ jQuery(function($) {
 	});	
 
 
+	$("input[name='is_private']").click(function(){
+		if ($(this).is(":checked")){
+			$('#privateModal').modal('show');
+		}
+	});
+
+	$("#save-private").click(function(){
+		$('#privateModal').modal('hide');
+	});
+
+	$("button#advanced").click(function(event){
+		event.preventDefault();
+		$("#advanced-fields").slideToggle();
+	});
+
+	$("#cancel-private").click(function(){
+		$('#privateModal').modal('hide');
+		$("input[name='is_private']").attr('checked', false).removeAttr("checked");;
+	});
+
 	// Form Submit
 	$('#analyze-form').submit(function(event){
 		event.preventDefault();
 
-		showWaitingMessage();
 		var url = $('form input[type="url"]').val();
 
-		$.ajax({url: "http://127.0.0.1:8080", 
-				type: "POST", 
-				data: JSON.stringify({"url": url}), 
-				dataType: "json"})
-		.done(function() {
-			// TODO: Redirect to analysis result
-		}).fail(function(request, status, error) {
-			console.log( "Theare was an error when processing your request: ");
-		});
+		sendRequest({"url": url});
+		showWaitingMessage();
 
 		return false;
 	});
+
+	function sendRequest(data){
+		$.ajax({url: "/", 
+				type: "POST", 
+				data: data})
+		.done(function() {
+			setTimeout(function(){
+				window.location.href = "/stats.html";
+			}, 3000);
+		}).fail(function(request, status, error) {
+			alert( "Theare was an error when processing your request");
+		});
+	}
 
 	function showWaitingMessage(){
 		$("#wait-message").removeClass("hide");
