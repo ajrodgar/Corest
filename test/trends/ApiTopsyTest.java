f2eaa8822c68af5918002bf193bd0ec4effa8db3
Óscar Antonio González de Chaves Pérez <oscar.agcp@gmail.com>
2014-05-07 14:11:20 +0100

    Some improvements were added

--- test/trends/ApiTopsyTest.java
+++ test/trends/ApiTopsyTest.java
@@ -1,24 +1,25 @@
 package trends;
 
-import com.maruti.otterapi.TopsyConfig;
+import com.maruti.otterapi.search.Response;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ApiTopsyTest {
-    
+
     @Test
     public void testSearchCount() {
-        TopsyConfig config = new TopsyConfig();
-        config.setApiKey("09C43A9B270A470B8EB8F2946A9369F3");
-        config.setSetProxy(false);
-        config.setProxyHost("hydproxy.igate.com");
-        config.setProxyPort("8080");
-        
-        String q1 = "Gran Canaria";
-        String q2 = "Tenerife";
         ApiTopsy instance = new ApiTopsy();
-        instance.searchCount(config, q1);
-        instance.searchCount(config, q2);
+
+        String query = "Gran Canaria";
+        Response response = instance.searchCount(query);
+
+        System.out.println("Number of tweets about \"" + query + "\" in the last:");
+        System.out.println("Hour: " + response.getH());
+        System.out.println("Day: " + response.getD());
+        System.out.println("Week: " + response.getW());
+        System.out.println("Month: " + response.getM());
+        System.out.println("Year: " + response.getA());
+
     }
 
 }
