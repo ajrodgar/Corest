a4d4df601be21a80bbcaed11ff653680483a1bbd
usuario <usuario@10.22.147.246>
2014-05-07 11:00:00 +0100

    method added to create a list with the name of classes and its frequencies

--- src/TwitterSearch/ClassFrequency.java
+++ src/TwitterSearch/ClassFrequency.java
@@ -1,5 +1,22 @@
 package TwitterSearch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassFrequency {
     
+    public List getNumTweetsOfClasses(){
+        List<String> listWords = new ArrayList<>(); //Here I will use a method to get the keywords.
+        List<KeyWordTweets> wordsFrequency = new ArrayList<>(); 
+        
+        for (String words : listWords) {
+            int frequency = numTweets(words);
+            KeyWordTweets WordTweets = new KeyWordTweets(words, frequency);
+            for (int i = 0; i < wordsFrequency.size(); i++) {
+                
+            }
+            wordsFrequency.add(WordTweets);
+        }
+        return wordsFrequency;
+    }
 }
