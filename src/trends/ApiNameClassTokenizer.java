e43eaa40b6cf2728e9d54369138ad6a6a6a1e81b
usuario <usuario@10.22.147.145>
2014-05-07 10:21:43 +0100

    Created ApiNameClassTokenizer class

---
+++ src/trends/ApiNameClassTokenizer.java
@@ -0,0 +1,13 @@
+package trends;
+
+import java.util.ArrayList;
+
+public class ApiNameClassTokenizer {
+
+    public ArrayList<String> tokens(String listClass) {
+        ArrayList<String> tokens = new ArrayList();
+        
+        return null;
+    }
+    
+}
