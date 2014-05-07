daa7e2fd7935eafc90ef48ed0300d346bb0c476d
Informaticos <Informaticos@10.44.13.219>
2014-05-07 11:53:43 +0100

    ApiClassNameTokenizer Ready

---
+++ src/trends/ApiClassNameTokenizer.java
@@ -0,0 +1,24 @@
+package trends;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiClassNameTokenizer {
+
+
+    public ArrayList<String> tokens(List<String> listClass) {
+        ArrayList<String> listToken = new ArrayList();
+
+        for (String nameClass : listClass) {
+            listToken.addAll(tokenListFromClassName(nameClass));
+        }
+        
+        return listToken;
+    }
+
+    private List<String> tokenListFromClassName(String nameClass) {
+        return Arrays.asList(nameClass.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])"));
+    }
+
+}
